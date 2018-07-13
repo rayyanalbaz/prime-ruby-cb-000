5d0c219aee8c7425994cbef2149e8f45d29764d6
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 14:07:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,13 @@
 # Add  code here!
 def prime?(number)
         i = 2
-      if number < 2 
+      if number < 2
         return false
       end
 
       if number % i == 0
         return  true
-        else
-        i=i+1
       end
+      i=i+1
 
 end
