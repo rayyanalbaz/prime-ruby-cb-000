f88263d904c8a1d3cbf4e449fb5c1e838bae9b6e
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 13:45:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(number)
-  
-    for i in 2..n-1 
+
+    for i in 2..n-1
       if n % i == 0
         false
       end
-      true 
+      true
   end
-  
-  
+
+
 end
