a88e0e411599c6e42bc2714dccfd8b6ad031130a
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 14:41:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -25,7 +25,7 @@ def prime?(number)
         while i * i <= number
             if number % i == 0
                 return false
-              end
+              else
 
             i += w
             w = 6 - w
