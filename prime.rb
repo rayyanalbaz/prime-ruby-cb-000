62d08c8c449f0cf39c20890623dce86624876ab8
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 14:06:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,14 @@
 # Add  code here!
 def prime?(number)
-  i = 2
-  while number > 1
+        i = 2
+      if number < 2 
+        return false
+      end
 
-if number % i == 0
-  return  true
-else
-  return false
-  i=i+1
-end
-
-end
+      if number % i == 0
+        return  true
+        else
+        i=i+1
+      end
 
 end
