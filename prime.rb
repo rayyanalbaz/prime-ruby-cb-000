765ad1489a8d7d2992db9d7a0a6f5e965269bac9
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-07-13 15:01:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+def prime?(number)
+        i = 2
+      if number < 2
+        return false
+      end
+      if number == 3
+        return true
+      end
+      if number == 2
+          return true
+      end
+
+      while i*i < i*number
+        if number % i == 0
+          return false
+        end
+        i=i+1
+      end
+      while i
+        if number % i != 0
+          return true
+        end
+        i=i+1
+      end
+
+
+end
